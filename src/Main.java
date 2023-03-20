354408522adf58e29f66142913a6656789a88781
MN-Simform <nimit@simformsolutions.com>
2023-03-20 13:29:30 +0530

    Modify code format and use better syntax

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,26 @@
 public class Main {
+    static String str1;
+    static String str2;
+    static StringBuilder sBuilder;
+    static StringBuffer sBuffer;
     public static void main(String[] args) {
-        System.out.println("String Variables Initialization :");
-        String str1 = "  String Using Literal  ";
-        String str2 = new String("  String Using New Keyword  ");
-        StringBuilder sBuilder = new StringBuilder("  String Builder ");
-        StringBuffer sBuffer = new StringBuffer("  String Buffer ");
 
+        str1 = "  String Using Literal  ";
+        str2 = new String("  String Using New Keyword  ");
+        sBuilder = new StringBuilder("  String Builder ");
+        sBuffer = new StringBuffer("  String Buffer ");
+
+        System.out.println("Initialised String Variables : ");
 //        Printing Strings Values in Console
-        System.out.println(str1);
-        System.out.println(str2);
-        System.out.println(sBuilder);
-        System.out.println(sBuffer);
+        System.out.println(str1 + "\n" + str2 + "\n" + sBuffer + "\n" + sBuffer);
         System.out.println();
 
 //        Append Lines in StringBuilder
-        sBuilder.append("Line 1 ");
-        sBuilder.append("Line 2 ");
-        sBuilder.append("Line 3 ");
-        sBuilder.append("Line 4 ");
-        sBuilder.append("Line 5 ");
+        sBuilder.append("Line 1 ").append("Line 2").append("Line 3").append("Line 4").append("Line 5");
         System.out.println("Appended String Builder : " + sBuilder);
 
 //        Append Lines in StringBuffer
-        sBuffer.append("Line 1 ");
-        sBuffer.append("Line 2 ");
-        sBuffer.append("Line 3 ");
-        sBuffer.append("Line 4 ");
-        sBuffer.append("Line 5 ");
+        sBuffer.append("Line 1 ").append("Line 2").append("Line 3").append("Line 4").append("Line 5");
         System.out.println("Appended String Buffer : " + sBuffer);
         System.out.println();
 
